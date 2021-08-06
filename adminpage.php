9fc278d896e67c09b5ce299625dc082c6bae8691
vikky75 <88150019+vikky75@users.noreply.github.com>
2021-08-07 02:17:58 +0530

    Add files via upload

--- adminpage.php
+++ adminpage.php
@@ -1,6 +1,7 @@
 <html>
 <head>
-<link rel="stylesheet" href="style.css">
+
+<link rel="stylesheet" href="login.css">
 <script>
 function showUser(str) {
   if (str == "") {
@@ -20,31 +21,32 @@ function showUser(str) {
 </script>
 </head>
 <body>
-
-<div class="heading"><h2>Hi ADMIN welcome back<h2></div>
-
-<div class="total">
-  <div class="option">
+  <div class="container">
+  <div class="heading"><h2>Hi Admin welcome back<h2></div>
+  <div class="nam">
     <form>
-      <select name="users" onchange="showUser(this.value)">
-         <option value="">view report</option>
-         <option value="Name1" name="emp">Name1</option>
-         <option value="Name2">Name2</option>
-         <option value="Name3">Name3</option>
-         <option value="Name4">Name4</option>
-      </select>
+      <select name="users"  onchange="showUser(this.value)">
+       <option value="">view report</option>
+        <option value="Name1">Name1</option>
+        <option value="Name2">Name2</option>
+        <option value="Name3">Name3</option>
+       <option value="Name4">Name4</option>
+     </select>
     </form>
+    <div id="txtHint" class=""><b>Report will be generated here..</b></div>
+    <br>
   </div>
-<div classs="but">
-<input type="button" value="Add user" name="add">
-<input type="button" value="Remove" name="remove">
-</div>
-</div>
-<div class ="grap"></div>
+  
+<div class="but">
 
-
+  <div><input type="button" value="Add user" class="butt" name="add"></div><br>
+  <div><input type="button" value="Remove" class="butt" name="remove"></div><br>
+  <div><input type="button" value="view progress" class="butt" name="view"></div>
+</div>
+<div class ="grap">
+  </div>
+</div>
 <br>
-<div id="txtHint"><b></b></div>
 
 </body>
 </html>
